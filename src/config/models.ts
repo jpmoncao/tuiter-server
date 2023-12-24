ec438e32d30bee56715f5156ebebf224b8237b01
jpmoncao <pedrohjoao44@gmail.com>
2023-12-24 00:51:33 -0300

    ✨feat: Add model to user followers

--- src/config/models.ts
+++ src/config/models.ts
@@ -1,4 +1,5 @@
 import { Users } from "../models/Users.js";
+import { UsersFollowers } from "../models/UsersFollowers.js";
 import { Tuites } from "../models/Tuites.js";
 import { TuitesComments } from "../models/TuitesComments.js";
 import { TuitesLikes } from "../models/TuitesLikes.js";
@@ -7,6 +8,7 @@ import { TuitesRetuites } from "../models/TuitesRetuites.js";
 export const models = {
     sync: function () {
         Users.sync();
+        UsersFollowers.sync();
         Tuites.sync();
         TuitesComments.sync();
         TuitesLikes.sync();
